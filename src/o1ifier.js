ff1406dc3921a534e7f00364b1dad4fd768c02a0
mfregoso <mfregoso@users.noreply.github.com>
2019-06-12 23:25:21 -0700

    use arrays to chain key value collisions

--- src/o1ifier.js
+++ src/o1ifier.js
@@ -1,13 +1,21 @@
 const get = require("./get");
 
-const o1ifier = (objsArr, keyPathName) => {
+const o1ifier = (objsArr, keyPathName, overwrite) => {
   if (!Array.isArray(objsArr)) throw new Error("Not an array");
 
-  const o1indexer = (o1index, obj) => {
+  const o1indexer = (o1index, obj, idx) => {
     const keyValue = get(obj, keyPathName);
     if (keyValue !== undefined) {
+      const o1Obj = {key: keyValue, obj, idx};
       if (o1index[keyValue] === undefined) {
-        o1index[keyValue] = obj;
+        o1index[keyValue] = [o1Obj];
+      } else if (get(o1index[keyValue][0], "key") !== keyValue && get(o1index[keyValue][1], "key") !== keyValue) {
+        o1index[keyValue].push(o1Obj);
+      } else if (overwrite) {
+        const store = o1index[keyValue];
+        for (let i = 0; i < store.length; i++) {
+          if (store[i].key === keyValue) store[i].obj = obj;
+        }
       }
     }
     return o1index;
@@ -16,7 +24,12 @@ const o1ifier = (objsArr, keyPathName) => {
   const o1index = objsArr.reduce(o1indexer, {});
 
   const o1lookup = (key) => {
-    return o1index[key];
+    const store = o1index[key];
+    if (Array.isArray(store)) {
+      for (let i = 0; i < store.length; i++) {
+        if (store[i].key === key) return store[i].obj;
+      }
+    }
   }
 
   return o1lookup;
