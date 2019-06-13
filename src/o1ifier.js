249c185608a141daae28d8a806febf98609695b1
mfregoso <mfregoso@users.noreply.github.com>
2019-06-12 21:54:40 -0700

    remove Map option

--- src/o1ifier.js
+++ src/o1ifier.js
@@ -3,27 +3,20 @@ const get = require("./get");
 const o1ifier = (objsArr, keyName) => {
   if (!Array.isArray(objsArr)) throw new Error("Not an array");
 
-  let hasMap = true;
-  try {new Map()}
-  catch(e) {hasMap = false;}
-
-  const index = hasMap ? new Map() : {};
+  const index = {};
 
   for (let i = 0; i < objsArr.length; i++) {
     const obj = objsArr[i];
     const keyValue = get(obj, keyName);
     
     if (keyValue !== undefined) {
-      if (hasMap && !index.has(keyValue)) {
-        index.set(keyValue, obj);
-      } else if (!hasMap && index[keyValue] === undefined) {
+      if (index[keyValue] === undefined) {
         index[keyValue] = obj;
       }
     }
   }
 
   const getItem = (key) => {
-    if (hasMap) return index.get(key);
     return index[key];
   }
 
