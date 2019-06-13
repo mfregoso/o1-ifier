9151c73cb34e3c74a34fda2d97d73a5dd70feb2f
mfregoso <mfregoso@users.noreply.github.com>
2019-06-12 22:27:55 -0700

    clean up test code

--- src/o1ifier.js
+++ src/o1ifier.js
@@ -3,15 +3,14 @@ const get = require("./get");
 const o1ifier = (objsArr, keyPathName) => {
   if (!Array.isArray(objsArr)) throw new Error("Not an array");
 
-  const o1indexer = (o1index, obj, idx) => {
+  const o1indexer = (o1index, obj) => {
     const keyValue = get(obj, keyPathName);
     if (keyValue !== undefined) {
       if (o1index[keyValue] === undefined) {
-        obj.idx = idx;
         o1index[keyValue] = obj;
       }
     }
-    return index;
+    return o1index;
   };
 
   const o1index = objsArr.reduce(o1indexer, {});
